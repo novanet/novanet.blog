af837f61432d6adf93ef16a3b3b2bd9e1f770401
Hallstein Brøtan <hallstein.brotan@novanet.no>
2022-02-09 10:45:56 +0100

    fix logging

--- [2022-02] Secretless access to Azure Service Bus/SecretlessQueueReader/ProcessQueueItem.cs
+++ [2022-02] Secretless access to Azure Service Bus/SecretlessQueueReader/ProcessQueueItem.cs
@@ -1,15 +1,17 @@
-using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
 namespace QueueReader
 {
-    public class ProcessQueueItem
+public class ProcessQueueItem
+{
+    private readonly ILogger _logger;
+    public ProcessQueueItem(ILoggerFactory loggerFactory) => _logger = loggerFactory.CreateLogger<ProcessQueueItem>();
+
+    [Function("ProcessQueueItem")]
+    public void Run([ServiceBusTrigger("%QueueName%", Connection = "ServiceBusConnection")] string myQueueItem)
     {
-        [Function("ProcessQueueItem")]
-        public void Run([ServiceBusTrigger("%QueueName%", Connection = "ServiceBusConnection")] string myQueueItem, ILogger _logger)
-        {
-            _logger.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
-        }
+        _logger.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
     }
+}
 }
