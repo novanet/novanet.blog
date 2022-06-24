a3120f78ef1d256b91daad21daf28de0545d5f96
Hallstein Brøtan <hallstein.brotan@novanet.no>
2022-06-24 14:03:51 +0200

    rename to new folder structure

---
+++ 2022-06_Hosting_Docker_Containers_in_Azure_App_Service/src/Service/webapi-in-docker-service/WeatherForecast.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace webapi_in_docker_service;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
