9de70f81d86d09feceb2a6b20815b465ce541ef7
Hallstein Brøtan <hallstein.brotan@novanet.no>
2023-02-24 08:43:54 +0100

    Code examples for Errors-to-Slack blogpost

---
+++ 2023-02_Errors_to_Slack/AlertHandler/Facade/ApplicationInsights/LogAlertParser.cs
@@ -0,0 +1,27 @@
+﻿namespace AlertHandler.Facade.ApplicationInsights
+{
+    public static class LogAlertParser
+    {
+        public static string LinkToSearchResults(this LogAlert alert)
+        {
+            var allOf = alert.data.alertContext.condition.allOf[0];
+            return allOf.linkToSearchResultsUI.Replace("\n", "");
+        }
+
+        public static string? ExceptionMessage(this LogAlert alert)
+        {
+            var dimension = alert!.GetDimension("outerMessage");
+
+            if (dimension is not null && dimension.Contains("\n"))
+                dimension = dimension.Split("\n")[0];
+
+            return dimension;
+        }
+
+        public static string? CloudRoleName(this LogAlert alert)
+            => alert!.GetDimension("cloud_RoleName");
+
+        private static string? GetDimension(this LogAlert alert, string dimensionName)
+            => alert!.data.alertContext.condition.allOf[0].dimensions.FirstOrDefault(x => x.name == dimensionName)?.value;
+    }
+}
---
+++ 2023-02_Errors_to_Slack/App/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Requires nuget Microsoft.ApplicationInsights.AspNetCore
+builder.Services.AddApplicationInsightsTelemetry();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => 
+{
+    throw new Exception(
+        "This text should appear in Slack!");
+});
+
+app.MapGet("/error", () =>
+{
+    throw new NotImplementedException(
+       "This is another text!");
+});
+
+app.Run();
